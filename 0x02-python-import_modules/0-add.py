1e4a4e70319a3e1de073b3a7a8ef5d2b82a29273
geekmeba <mebratuhailu54@gmail.com>
2022-06-02 13:17:39 -0700

    add func

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-result = add_0.add(a, b)
-print(f'{a:d} + {b:d} = {result:d}')
+from add_0 import add
+if __name__ == "__main__":
+ a = 1
+ b = 2
+print(f'{a:d} + {b:d} = {add(a,b):d}')
